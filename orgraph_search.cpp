58da3d2705e7d457bd3d5f718ca75342bae766e8
Sw0rdPenc1l <34251020+Sw0rdPenc1l@users.noreply.github.com>
2017-12-06 21:56:13 +0200

    Update orgraph_search.cpp

--- orgraph_search.cpp
+++ orgraph_search.cpp
@@ -2,9 +2,9 @@
 //В этом файле реализована функция, которая выполняет нашу задачу.
 void CheckDistance(Graph graph, unsigned distance, unsigned vertex)
 {//Функция, которая выводит все вершины, из которых существует путь в выбранную.
-    unsigned ** pathCounts = SetMatrix(graph.GetNumberOfVerticies());
+    unsigned ** pathCounts = SetMatrix(graph.GetNumberOfVerticies()); //Матрица, в которой будет записанно из каких в какие вершины можно попасть за distance ребер.
     for(unsigned k = 0; k < distance; k++)
-    {
+    {//Собственно, что-бы узнать какие вершины за путь k проходят к каким то вершинам. Нужно просто возвести матрицу смежности в степень k. Мы получим матрицу в которой будет видно в какие вершины из каких можно попасть пройдя k ребер.
         for(unsigned i = 0; i < graph.GetNumberOfVerticies(); i++)
         {
             for(unsigned j = 0; j < graph.GetNumberOfVerticies(); j++)
@@ -12,7 +12,7 @@ void CheckDistance(Graph graph, unsigned distance, unsigned vertex)
         }
     }
     for(unsigned i = 0; i < graph.GetNumberOfVerticies(); i++)
-    {
+    {//Просто выводим те вершины, что ведут в выбранную. Просто нужно выписать в столбе все вершины, что дают true в столбе под номером выбранной вершины.
         if(pathCounts[i][--vertex])
             cout << i++ << " ";
     }
