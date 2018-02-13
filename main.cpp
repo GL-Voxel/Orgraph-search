d7269cfa95bcf8759cea68166fec24c6433a43c1
Sw0rdPenc1l <34251020+Sw0rdPenc1l@users.noreply.github.com>
2018-02-13 16:23:39 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "orgrash_search.cpp";
+
+bool main()
+{
+  Graph graph;
+  unsinged int vertex = 0, 
+    path = 0;
+  cin >> graph;
+  cout << graph << "\n" << "Введите номер вершины, что вы желаете проверить: ";
+  cin << vetrex;
+  cout << "Введите расстояние, которое следует пройти к этой вершине: ";
+  cin << path;
+  system("pause");
+  return false;
+}
